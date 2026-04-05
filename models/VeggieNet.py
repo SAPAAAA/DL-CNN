b674a8d25d387db104fc995b59d67e93de367fd0
SAPAAAA <78519771+SAPAAAA@users.noreply.github.com>
2026-04-05 23:09:08 +0700

    refactor: streamline VeggieNet architecture by consolidating layers into sequential blocks

--- models/VeggieNet.py
+++ models/VeggieNet.py
@@ -8,47 +8,41 @@ class VeggieNet(nn.Module):
     def __init__(self, num_classes, image_size):
         super(VeggieNet, self).__init__()
                 
-        self.pw = nn.Conv2d(in_channels=3, out_channels=256, kernel_size=1, padding=0)
-                
-        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
-        
-        self.conv = nn.Conv2d(3, 64, kernel_size=3, padding=1)   
-        
-        self.dw1 = nn.Conv2d(64, 64, kernel_size=3, stride=2, padding=1, groups=64)
-        self.pw1 = nn.Conv2d(64, 128, kernel_size=1, padding=0)
-        
-        self.dw2 = nn.Conv2d(128, 128, kernel_size=5, stride=2, padding=2, groups=128)
-        self.pw2 = nn.Conv2d(128, 256, kernel_size=1, padding=0)
-        
-        self.seq = nn.Sequential(
-            self.conv,
+        self.seq1 = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, padding=1),
             nn.BatchNorm2d(64),
             nn.ReLU(),
-            self.pool,
-            self.dw1,
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(64, 64, kernel_size=3, stride=2, padding=1, groups=64),
             nn.BatchNorm2d(64),
-            self.pw1,
+            nn.Conv2d(64, 128, kernel_size=1, padding=0),
             nn.BatchNorm2d(128),
             nn.ReLU(),
-            self.dw2,
+            nn.Conv2d(128, 128, kernel_size=5, stride=2, padding=2, groups=128),
             nn.BatchNorm2d(128),
-            self.pw2,
+            nn.Conv2d(128, 256, kernel_size=1, padding=0),
             nn.BatchNorm2d(256),
             nn.ReLU()
         )
         
-        self.size = image_size // 8
+        self.seq2 = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.Conv2d(64, 256, kernel_size=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.AdaptiveAvgPool2d((28, 28))
+        )
         
         self.pdp = PDPNet(in_channels=512, image_size=28, num_classes=num_classes)
         
 
     def forward(self, x):
-        batch1 = self.seq(x)
-
-        batch2 = self.pw(x)
-        batch2 = F.adaptive_avg_pool2d(batch2, (batch1.size(2), batch1.size(3)))
+        batch1 = self.seq1(x)
+        batch2 = self.seq2(x)
         
-        x = torch.cat((batch1, batch2), dim=1) # C = 256 + 256
+        x = torch.cat((batch1, batch2), dim=1)
         
         x = self.pdp(x)        
         return x
