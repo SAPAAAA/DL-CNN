7e044da67d731e5371fac35ab16511f0c2c391ba
SAPAAAA <78519771+SAPAAAA@users.noreply.github.com>
2026-04-05 23:39:33 +0700

    trying something

--- models/VeggieNet.py
+++ models/VeggieNet.py
@@ -7,42 +7,21 @@ from models.PDPNet import PDPNet
 class VeggieNet(nn.Module):
     def __init__(self, num_classes, image_size):
         super(VeggieNet, self).__init__()
+        
+        size = (image_size - 2) // 4
                 
-        self.seq1 = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=3, padding=1),
-            nn.BatchNorm2d(64),
+        self.seq = nn.Sequential(
+            nn.Conv2d(3, 128, kernel_size=2, padding=0),
+            nn.Conv2d(128, 64, kernel_size=2, padding=0),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(64, 64, kernel_size=3, stride=2, padding=1, groups=64),
-            nn.BatchNorm2d(64),
-            nn.Conv2d(64, 128, kernel_size=1, padding=0),
-            nn.BatchNorm2d(128),
+            nn.Conv2d(64, 32, kernel_size=1, padding=0),
             nn.ReLU(),
-            nn.Conv2d(128, 128, kernel_size=5, stride=2, padding=2, groups=128),
-            nn.BatchNorm2d(128),
-            nn.Conv2d(128, 256, kernel_size=1, padding=0),
-            nn.BatchNorm2d(256),
-            nn.ReLU()
-        )
-        
-        self.seq2 = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=3, padding=1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-            nn.Conv2d(64, 256, kernel_size=1),
-            nn.BatchNorm2d(256),
-            nn.ReLU(),
-            nn.AdaptiveAvgPool2d((28, 28))
-        )
-        
-        self.pdp = PDPNet(in_channels=512, image_size=28, num_classes=num_classes)
-        
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Flatten(),
+            nn.Linear(32 * size ** 2, num_classes)
+        )        
 
     def forward(self, x):
-        batch1 = self.seq1(x)
-        batch2 = self.seq2(x)
-        
-        x = torch.cat((batch1, batch2), dim=1)
-        
-        x = self.pdp(x)        
+        x = self.seq(x)        
         return x
